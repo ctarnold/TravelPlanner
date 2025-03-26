68068bc5c7470b1a94a4bb0c2f28e431c8ccb502
Christian Arnold <ctarnold@princeton.edu>
2025-03-25 23:41:15 -0400

    put this on two gpu, memory scarce

--- agents/tool_agents.py
+++ agents/tool_agents.py
@@ -479,7 +479,7 @@ class ReactAgent:
                 if self.react_name == 'gemini':
                     request = format_step(self.llm.invoke(self._build_agent_prompt(),stop=['\n']).content)
                 elif self.react_name == 'local':
-                    request = format_step(self.llm(prompt = prompt, stop_list=['\n']))
+                    request = format_step(self.llm(prompt = prompt, stop_list=['\n'], max_length=256).content)
                 elif isinstance(self.llm, ChatOpenAI):
                     request = format_step(self.llm([HumanMessage(content=self._build_agent_prompt())]).content)
                 else:
