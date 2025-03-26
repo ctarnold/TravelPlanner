51ac25cd0986f23cd617d04698deff4dd7c566e6
Christian Arnold <ctarnold@princeton.edu>
2025-03-26 19:47:49 -0400

    try refinement reset false

--- agents/manager_agent.py
+++ agents/manager_agent.py
@@ -62,7 +62,7 @@ class ManagerAgent:
         refinement_prompt = f"Current plan:\n{plan}\n\nImprove the plan for the included query based on the attached feedback. Do not change aspects outside of the feedback.\n" + "query: \n" + self.query + "\nfeedback:\n "
         refinement_prompt = refinement_prompt + feedback
         print("\nREFINING PLAN\n", flush = True)
-        refined_plan, _, json = self.react_agent.run(refinement_prompt, reset=True) # Do I want true or false? True new agent, false keeps mems+ context.
+        refined_plan, _, json = self.react_agent.run(refinement_prompt, reset=False) # Do I want true or false? True new agent, false keeps mems+ context.
         
         return refined_plan, str(json)
 
@@ -96,6 +96,7 @@ class ManagerAgent:
 
             new_plan, new_json = self.refine_plan(plan, evaluations)
             if new_plan is not None and len(new_plan) != 0:
+                print("\n\n used new plan \n\n", flush=True)
                 plan = new_plan
                 json = new_json
 
