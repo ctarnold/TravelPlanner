e90a7ba1b5cdc7faca7dc62994fdf4022b3c2f24
Christian Arnold <ctarnold@princeton.edu>
2025-03-25 23:15:52 -0400

    fix blank space

--- agents/tool_agents.py
+++ agents/tool_agents.py
@@ -154,7 +154,7 @@ class ReactAgent:
              # C:\Users\chris\OneDrive\Desktop\SeniorThesisCode\agents\models\Qwen2.5-0.5B-Instruct
              # ../../agents/models/Qwen2.5-0.5B-Instruct
              #  Llama-3.1-8B-Instruct 
-            name = "/scratch/gpfs/ca2992/models/Llama-3.1-8B-Instruct "
+            name = "/scratch/gpfs/ca2992/models/Llama-3.1-8B-Instruct"
             self.llm = LocalModel(model_path=name) 
             self.llm.name=name 
             print("Managed LLM: ", self.llm.name)
