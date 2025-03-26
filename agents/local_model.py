8c900006c1153a4eaa83e2d3af0617c787492bb6
Christian Arnold <ctarnold@princeton.edu>
2025-03-26 16:43:01 -0400

    cut token count

--- agents/local_model.py
+++ agents/local_model.py
@@ -68,7 +68,7 @@ class LocalModel:
                 model=self.model,
                 tokenizer= self.tokenizer,
                 device_map=self.device,
-                max_new_tokens = 30,
+                max_new_tokens = 10,
                 do_sample=True,
                 return_full_text=False,
                 top_k=10,
