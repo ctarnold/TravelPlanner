941b67e9ee2e63775e320806e2460f7a1a95ba76
Christian Arnold <ctarnold@princeton.edu>
2025-03-27 00:16:56 -0400

    use QwQ-32B planner

--- tools/planner/apis.py
+++ tools/planner/apis.py
@@ -89,7 +89,8 @@ class Planner:
             # self.llm = LocalModel(model_path = "/scratch/gpfs/ca2992/models/DeepSeek-R1-Distill-Llama-8B")
             # self.llm = LocalModel(model_path = "/scratch/gpfs/ca2992/models/Llama-3.1-8B-Instruct", mode="planning")
             # self.llm = LocalModel(model_path = "../../agents/models/Qwen2.5-0.5B-Instruct")
-            self.llm = LocalModel(model_path = "/scratch/gpfs/ca2992/models/Llama-3.1-8B-Instruct-travelplanner-SFT")
+            # self.llm = LocalModel(model_path = "/scratch/gpfs/ca2992/models/Llama-3.1-8B-Instruct-travelplanner-SFT")
+            self.llm = LocalModel(model_path = "/scratch/gpfs/ca2992/models/QwQ-32B")
             self.max_token_length = 30000
 
 
