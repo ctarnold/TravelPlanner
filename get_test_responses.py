6fafa4d3fb810821e7206112bd411bc8b102369e
Christian Arnold <ctarnold@princeton.edu>
2025-03-29 20:16:51 -0400

    fix dict string issue

--- get_test_responses.py
+++ get_test_responses.py
@@ -47,6 +47,7 @@ def run_slice_and_write(agent: ManagerAgent, index):
             print(str(item) + " \n", file = f)
     results = []
 
+
 def main(args):
     print(args[1], flush=True)
     agent = ManagerAgent(query = "", tools= tools_list)
