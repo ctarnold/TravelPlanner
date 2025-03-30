289a2ebd7093c7bf024c7accacb58fac339ace39
Christian Arnold <ctarnold@princeton.edu>
2025-03-30 16:57:18 -0400

    try with val item

--- agents/local_model.py
+++ agents/local_model.py
@@ -109,9 +109,8 @@ class LocalModel:
             print(inputs["input_ids"], flush=True)
             for vector in inputs["input_ids"]:
                 for val in vector:
-                    for item in val:
-                        if item < 0:
-                            print(item, flush=True)
+                    if val.item() < 0:
+                        print(val.item(), flush=True)
 
             if self.mode == 'tool_calling':
                 print("\ntool hf reached\n", flush=True)
